34610391cb0017b6ae848f9acc27892f661a23a8
Jakob Bussas <jakobbu@gmx.de>
2018-02-21 19:21:33 +0100

    Initial Commit

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
