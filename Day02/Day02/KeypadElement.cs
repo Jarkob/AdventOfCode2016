552d2c7868937de7d425f111238e0a591db1a13c
Jakob Bussas <jakobbu@gmx.de>
2018-02-22 21:21:00 +0100

    Day02 Part1

---
+++ Day02/Day02/KeypadElement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace Day02
+{
+    public class KeypadElement
+    {
+        public KeypadElement()
+        {
+
+        }
+
+        public KeypadElement(int Value)
+        {
+            this.Value = Value;
+        }
+
+        private KeypadElement up;
+        public KeypadElement Up
+        {
+            get { return this.up; }
+            set
+            {
+                this.up = value;
+
+                if (value.Down == null)
+                {
+                    value.Down = this;
+                }
+            }
+        }
+
+        private KeypadElement right;
+        public KeypadElement Right
+        {
+            get { return this.right; }
+            set
+            {
+                this.right = value;
+
+                if (value.Left == null)
+                {
+                    value.Left = this;
+                }
+            }
+        }
+
+        private KeypadElement down;
+        public KeypadElement Down
+        {
+            get { return this.down; }
+            set
+            {
+                this.down = value;
+
+                if (value.Up == null)
+                {
+                    value.Up = this;
+                }
+            }
+        }
+
+        private KeypadElement left;
+        public KeypadElement Left
+        {
+            get { return this.left; }
+            set
+            {
+                this.left = value;
+
+                if (value.Right == null)
+                {
+                    value.Right = this;
+                }
+            }
+        }
+
+        public int Value { get; set; }
+    }
+}
--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Day02
 {
@@ -6,7 +8,75 @@ namespace Day02
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] Input = File.ReadAllLines("../../Input.txt");
+
+            // Test
+            //Input = null;
+            //Input = new string[] { "ULL", "RRDDD", "LURDL", "UUUUD" };
+
+            Dictionary<int, KeypadElement> Keypad = new Dictionary<int, KeypadElement>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                Keypad.Add(i, new KeypadElement(i));
+            }
+
+            Keypad[1].Right = Keypad[2];
+            Keypad[1].Down = Keypad[4];
+            Keypad[2].Right = Keypad[3];
+            Keypad[2].Down = Keypad[5];
+            Keypad[3].Down = Keypad[6];
+            Keypad[4].Right = Keypad[5];
+            Keypad[4].Down = Keypad[7];
+            Keypad[5].Right = Keypad[6];
+            Keypad[5].Down = Keypad[8];
+            Keypad[6].Down = Keypad[9];
+            Keypad[7].Right = Keypad[8];
+            Keypad[8].Right = Keypad[9];
+
+
+            string Code = "";
+            KeypadElement StartElement = Keypad[5];;
+
+            foreach (var Command in Input)
+            {
+                foreach (var Element in Command)
+                {
+                    switch (Element)
+                    {
+                        case 'U':
+                            if (StartElement.Up != null)
+                            {
+                                StartElement = StartElement.Up;
+                            }
+                            break;
+                        case 'R':
+                            if (StartElement.Right != null)
+                            {
+                                StartElement = StartElement.Right;
+                            }
+                            break;
+                        case 'D':
+                            if (StartElement.Down != null)
+                            {
+                                StartElement = StartElement.Down;
+                            }
+                            break;
+                        case 'L':
+                            if (StartElement.Left != null)
+                            {
+                                StartElement = StartElement.Left;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                Code += StartElement.Value.ToString();
+            }
+
+            Console.WriteLine("Part1: "+ Code); // 53255
         }
     }
 }
