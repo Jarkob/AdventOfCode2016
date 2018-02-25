5debd979ad5bce0667d431efdf15a0a6be7d3932
Jakob Bussas <jakobbu@gmx.de>
2018-02-25 10:30:15 +0100

    Day05

---
+++ Day05/Day05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day05
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
