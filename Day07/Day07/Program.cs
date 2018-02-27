560feddce9d94d203cb0428583ad8657a8e41331
Jakob Bussas <jakobbu@gmx.de>
2018-02-27 23:07:21 +0100

    Day07 Part1 finished

--- Day07/Day07/Program.cs
+++ Day07/Day07/Program.cs
@@ -13,18 +13,40 @@ namespace Day07
 
             for (int i = 0; i < Input.Length; i++)
             {
+                char LastBracket = ']';
+                bool InsideBrackets = false;
+                bool OutsideBrackets = false;
+
                 for (int j = 0; j < Input[i].Length - 3; j++)
                 {
-                    if (Input[i][j] == Input[i][j + 3] && Input[i][j + 1] == Input[i][j + 2]
-                        && Input[i][j] != ' ' && Input[i][j + 1] != ' ' && Input[i][j] != Input[i][j + 1])
+                    if (Input[i][j] == ']' || Input[i][j] == '[')
                     {
-                        Tls++;
-                        break;
+                        LastBracket = Input[i][j];
+                        continue;
                     }
+
+                    if (Input[i][j] == Input[i][j + 3] && Input[i][j + 1] == Input[i][j + 2]
+                        && Input[i][j] != ']' && Input[i][j + 1] != ']' && Input[i][j] != '[' && Input[i][j + 1] != '[' && Input[i][j] != Input[i][j + 1])
+                    {
+                        if (LastBracket == '[')
+                        {
+                            InsideBrackets = true;
+                            break;
+                        }
+                        else if (LastBracket == ']')
+                        {
+                            OutsideBrackets = true;
+                        }
+                    }
+                }
+
+                if (!InsideBrackets && OutsideBrackets)
+                {
+                    Tls++;
                 }
             }
 
-            Console.WriteLine("Part1: " + Tls);
+            Console.WriteLine("Part1: " + Tls); // 105
         }
     }
 }
