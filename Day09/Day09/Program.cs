bce3254438e8c41f0ecb46be61e1806451daba5b
Jakob Bussas <jakobbu@gmx.de>
2018-03-07 08:23:31 +0100

    Day09

---
+++ Day09/Day09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day09
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            File.ReadAllText("../../Input.txt");
+        }
+    }
+}
