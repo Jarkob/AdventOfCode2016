a7cf65eda4e1de8f0420635428e9614fac0698e6
Jakob Bussas <jakobbu@gmx.de>
2018-09-20 20:30:12 +0200

    worked on Day10

---
+++ Day10/Day10/Bot.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day10
+{
+    public class Bot
+    {
+        public Bot(Dictionary<int, Bot> bots, Dictionary<int, int> output, int id)
+        {
+            this.Bots = bots;
+            this.Output = output;
+            this.Id = id;
+        }
+
+        public void GiveMicrochip(int microchip)
+        {
+            if (this.Microchips[0] == -1)
+            {
+                this.Microchips[0] = microchip;
+            }
+            else if (this.Microchips[1] == -1)
+            {
+                this.Microchips[1] = microchip;
+            }
+            else
+            {
+                throw new Exception("Bot can only carry 2 microchips");
+            }
+
+            if (this.Microchips[0] != -1 && this.Microchips[1] != -1)
+            {
+                this.Execute();
+            }
+        }
+
+        public void GiveInstruction(bool lowBot, int lowId, bool highBot, int highId)
+        {
+            this.LowBot = lowBot;
+            this.LowId = lowId;
+            this.HighBot = highBot;
+            this.HighId = highId;
+
+            if (this.Microchips[0] != -1 && this.Microchips[1] != -1)
+            {
+                this.Execute();
+            }
+        }
+
+        private void Execute()
+        {
+            //if ((this.Microchips[0] == 5 && this.Microchips[1] == 2) || (this.Microchips[0] == 2 && this.Microchips[1] == 5))
+            if ((this.Microchips[0] == 61 && this.Microchips[1] == 17) || (this.Microchips[0] == 17 && this.Microchips[1] == 61))
+            {
+                Console.WriteLine("Part1: " + this.Id);
+            }
+
+            // debug
+            // give to output
+            if (!this.HighBot || !this.LowBot)
+            {
+                Console.WriteLine("KEIN BOT");
+            } else {
+                Console.WriteLine("BOT");
+            }
+
+            Console.WriteLine("EXECUTE");
+
+            if (this.Microchips[0] > this.Microchips[1])
+            {
+                if (this.LowBot)
+                {
+                    // give low to bot
+                    this.Bots[LowId].GiveMicrochip(this.Microchips[1]);
+                }
+                else
+                {
+                    // give low to output
+                    if (this.Output.ContainsKey(LowId))
+                    {
+                        this.Output[LowId] = this.Microchips[1];
+                    }
+                    else
+                    {
+                        this.Output.Add(LowId, this.Microchips[1]);
+                    }
+                }
+
+                if (this.HighBot)
+                {
+                    // give high to bot
+                    this.Bots[HighId].GiveMicrochip(this.Microchips[0]);
+                }
+                else
+                {
+                    // give high to output
+                    if (this.Output.ContainsKey(HighId))
+                    {
+                        this.Output[HighId] = this.Microchips[0];
+                    }
+                    else
+                    {
+                        this.Output.Add(HighId, this.Microchips[0]);
+                    }
+                }
+            }
+            else if (this.Microchips[0] < this.Microchips[1])
+            {
+                if (this.LowBot)
+                {
+                    // give low to bot
+                    this.Bots[LowId].GiveMicrochip(this.Microchips[0]);
+                }
+                else
+                {
+                    // give low to output
+                    if (this.Output.ContainsKey(LowId))
+                    {
+                        this.Output[LowId] = this.Microchips[0];
+                    }
+                    else
+                    {
+                        this.Output.Add(LowId, this.Microchips[0]);
+                    }
+                }
+
+                if (this.HighBot)
+                {
+                    // give high to bot
+                    this.Bots[HighId].GiveMicrochip(this.Microchips[1]);
+                }
+                else
+                {
+                    // give high to output
+                    if (this.Output.ContainsKey(HighId))
+                    {
+                        this.Output[HighId] = this.Microchips[1];
+                    }
+                    else
+                    {
+                        this.Output.Add(HighId, this.Microchips[1]);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Unexpected behavior: Bot executing but has equal value microchips");
+            }
+
+            // reset values
+            this.Microchips = new int[] { -1, -1 };
+        }
+
+        public int Id { get; set; }
+
+        private Dictionary<int, int> Output;
+
+        private Dictionary<int, Bot> Bots;
+
+        private int[] Microchips = new int[] { -1, -1 };
+
+        private bool LowBot;
+
+        private bool HighBot;
+
+        private int LowId;
+
+        private int HighId;
+
+        public override string ToString()
+        {
+            return String.Format("Bot {0}: microchips: ({1};{2})", this.Id, this.Microchips[0], this.Microchips[1]);
+        }
+    }
+}
---
+++ Day10/Day10/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] Input = File.ReadAllLines("../../input.txt");
+
+            // test
+            //Input = new string[] {
+            //    "value 5 goes to bot 2",
+            //    "bot 2 gives low to bot 1 and high to bot 0",
+            //    "value 3 goes to bot 1",
+            //    "bot 1 gives low to output 1 and high to bot 0",
+            //    "ot 0 gives low to output 2 and high to output 0",
+            //    "value 2 goes to bot 2"
+            //};
+
+            Dictionary<int, Bot> Bots = new Dictionary<int, Bot>();
+            Dictionary<int, int> Output = new Dictionary<int, int>();
+
+            string[] Instruction;
+            foreach (var element in Input)
+            {
+                Instruction = element.Split(' ');
+                if (Instruction[2] == "goes")
+                {
+                    // give value to bot
+                    int BotId = Convert.ToInt32(Instruction[5]);
+                    int Value = Convert.ToInt32(Instruction[1]);
+
+                    if (Bots.ContainsKey(BotId))
+                    {
+                        Bots[BotId].GiveMicrochip(Value);
+                    }
+                    else
+                    {
+                        Bots.Add(BotId, new Bot(Bots, Output, BotId));
+                        Bots[BotId].GiveMicrochip(Value);
+                    }
+                }
+                else if (Instruction[2] == "gives")
+                {
+                    // give bot instruction
+                    int BotId = Convert.ToInt32(Instruction[1]);
+                    bool LowBot = (Instruction[5] == "bot");
+                    int LowId = Convert.ToInt32(Instruction[6]);
+                    bool HighBot = (Instruction[10] == "bot");
+                    int HighId = Convert.ToInt32(Instruction[11]);
+
+                    if (Bots.ContainsKey(BotId))
+                    {
+                        Bots[BotId].GiveInstruction(LowBot, LowId, HighBot, HighId);
+                    }
+                    else
+                    {
+                        Bots.Add(BotId, new Bot(Bots, Output, BotId));
+                        Bots[BotId].GiveInstruction(LowBot, LowId, HighBot, HighId);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Instructions unclear: " + element);
+                }
+            }
+
+            ////// print bots
+            //foreach (var element in Bots)
+            //{
+            //    Console.WriteLine(element.Value);
+            //}
+
+            ////// print output
+            //foreach (var element in Output)
+            //{
+            //    Console.WriteLine(element);
+            //}
+        }
+    }
+}
